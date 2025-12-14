05a10d05b784c41398a06ef9e5c7c72e30c1d5ba
KOBOKOMASERO <kobokophilip@gmail.com>
2025-12-14 23:45:06 +0300

    urls

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -19,11 +19,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.http import JsonResponse
+
 urlpatterns = [
+    path('', lambda request: JsonResponse({'message': 'API is live!'})),
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/posts/', include('posts.urls')),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
